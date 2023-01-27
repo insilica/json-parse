f2324d4ea78f8d643ab01f1134f125a64152e599
John Shaffer <jdsha@proton.me>
2023-01-27 10:57:49 -0600

    Add build script

---
+++ build.clj
@@ -0,0 +1,29 @@
+(ns build
+  (:refer-clojure :exclude [test])
+  (:require [clojure.tools.build.api :as b]
+            [org.corfield.build :as bb]))
+
+(def lib 'co.insilica/json-parse)
+(def version "1.0.0")
+(defn get-version [opts]
+  (str version (when (:snapshot opts) "-SNAPSHOT")))
+
+(defn test [opts]
+  (-> opts
+      (assoc :main-args ["-m" "kaocha.runner"])
+      (bb/run-task [:test]))
+  opts)
+
+(defn ci "Run the CI pipeline of tests (and build the JAR)." [opts]
+  (-> opts
+      (assoc :lib lib :version (get-version opts))
+      bb/clean
+      test
+      bb/clean
+      (assoc :src-pom "template/pom.xml")
+      bb/jar))
+
+(defn deploy "Deploy the JAR to Clojars." [opts]
+  (-> opts
+      (assoc :lib lib :version (get-version opts))
+      bb/deploy))
